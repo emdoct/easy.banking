b79796f2835c455a14a574f75bcaa3fdf178b389
emdoct <62918095+emdoct@users.noreply.github.com>
2020-06-05 14:07:23 +1000

    add scroll animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(function() {
 	$('.menu-toggle').on('click', function(){
-		$('.mobile-menu').toggleClass('hide')
+		$('.mobile-menu').toggleClass('hide'),
+		$('.burger').toggleClass('cross');
 });
+
+	 $('.about').on('click', function (event) {
+
+	    	   let about = $(this).attr('href');
+	    	   let top = $(about).offset().top;
+	    	  $('body,html').animate({scrollTop: top}, 800);
+	    });
+
+	  $('.cont').on('click', function (event) {
+
+	    	   let cont = $(this).attr('href');
+	    	   let top = $(cont).offset().top;
+	    	  $('body,html').animate({scrollTop: top}, 1000);
+	    });
+
+	   $('.bl').on('click', function (event) {
+
+	    	   let bl = $(this).attr('href');
+	    	   let top = $(bl).offset().top;
+	    	  $('body,html').animate({scrollTop: top}, 1000);
+	    });
 });
